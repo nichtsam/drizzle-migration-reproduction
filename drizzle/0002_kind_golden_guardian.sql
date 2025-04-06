439c3a3df717e12cf9f39a9e856ae86dee37b5cf
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2025-04-06 15:11:26 +0200

    add object key on user image

---
+++ drizzle/0002_kind_golden_guardian.sql
@@ -0,0 +1,3 @@
+DROP TABLE `cache`;--> statement-breakpoint
+ALTER TABLE `user_image` ADD `object_key` text DEFAULT 'migration_needed' NOT NULL;--> statement-breakpoint
+CREATE INDEX `user_id_idx` ON `session` (`user_id`);
